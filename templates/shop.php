a00ae8cfc8666ce177dbeae3581c699e3e989d50
Sazedul Haque <sazedulhaque5@gmail.com>
2026-07-29 20:21:55 +0600

    move single product design in a different file

--- templates/shop.php
+++ templates/shop.php
@@ -4,6 +4,8 @@
  * Shop Page Template.
  *
  * @package Kirki\Ecommerce\Templates
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
  * @since 1.0.0
  */
 
@@ -12,26 +14,22 @@ defined('ABSPATH') || exit;
 use Kirki\Ecommerce\App\Helpers\TemplateHelper;
 use Kirki\Ecommerce\Wordpress\SiteRoute;
 
-$page     = SiteRoute::route_param('page', 1);
+$page = SiteRoute::route_param('page', 1);
 $products = SiteRoute::route_data('products', []);
 ?>
 
 <?php TemplateHelper::get_header(); ?>
 
-<div style="max-width:960px;margin:2rem auto;padding:0 1.5rem;">
-
+<div class="kirki-ecom-page-wrapper">
     <h1><?php echo esc_html__('Shop', 'kirki-ecommerce'); ?></h1>
 
-    <?php if (!empty($products)) : ?>
-        <div style="display:grid;grid-template-columns:repeat(auto-fill,minmax(220px,1fr));gap:1.25rem;margin-top:1.5rem;">
-            <?php foreach ($products as $product) : ?>
-                <?php TemplateHelper::load_template('shop/parts/product-card', ['product' => $product], false); ?>
-            <?php endforeach; ?>
-        </div>
-    <?php else : ?>
-        <p style="color:#71717a;"><?php echo esc_html__('No products found.', 'kirki-ecommerce'); ?></p>
-    <?php endif; ?>
-
+    <div class="kirki-ecom-products">
+        <?php
+        foreach ($products as $product) {
+            TemplateHelper::load_template('shop/parts/product-card', ['product' => $product], false);
+        }
+        ?>
+    </div>
 </div>
 
 <?php TemplateHelper::get_footer(); ?>
