16850af7f74f1ed4b1c5c2ede416abf4b6943c20
Sanjana Khan <sanjanakhan772@gmail.com>
2026-08-03 09:11:38 +0600

    settings field added

--- payments/kirki-authorizenet/src/Authorizenet.php
+++ payments/kirki-authorizenet/src/Authorizenet.php
@@ -2,6 +2,7 @@
 
 namespace Kirki\Ecommerce\Payments;
 
+use Exception;
 use Kirki\Ecommerce\App\Payment\PaymentGateway;
 
 defined('ABSPATH') || exit;
@@ -11,28 +12,53 @@ class Authorizenet extends PaymentGateway
     public function __construct()
     {
         $this->id = 'authorizenet';
-        $this->title = __('Stripe', 'kirki-ecommerce');
-        $this->description = __('Stripe payment gateway', 'kirki-ecommerce');
-        $this->icon = 'stripe';
-        $this->settings_key = 'stripe';
+        $this->title = __('AuthorizeNet', 'kirki-ecommerce');
+        $this->description = __('AuthorizeNet payment gateway', 'kirki-ecommerce');
+        $this->icon = 'authorizenet';
+        $this->settings_key = 'authorizenet';
         $this->is_manual = false;
-        $this->has_fields = false;
+        $this->has_fields = true;
 
         parent::__construct();
 
         $this->set_admin_fields([
             [
-                'name' => 'secret_key',
-                'label' => __('Secret Key', 'kirki-ecommerce'),
+                'name' => 'login_id',
+                'label' => __('Login ID', 'kirki-ecommerce'),
                 'type' => 'text',
                 'required' => true,
             ],
             [
-                'name' => 'webhook_secret',
-                'label' => __('Webhook Secret', 'kirki-ecommerce'),
+                'name' => 'transaction_key',
+                'label' => __('Transaction key', 'kirki-ecommerce'),
+                'type' => 'text',
+                'required' => true,
+            ],
+            [
+                'name' => 'signature_key',
+                'label' => __('Signature key', 'kirki-ecommerce'),
                 'type' => 'text',
                 'required' => true,
             ],
         ]);
+    }
+
+    /**
+     * Pay for an order.
+     *
+     * @param Order $order
+     * @return string
+     * @throws Exception
+     */
+    public function pay(Order $order)
+    {
+        if (!$this->enabled()) {
+            throw new Exception(__('AuthorizeNet is not enabled.', 'kirki-ecommerce'));
+        }
+
+        try {
+        } catch (Exception $e) {
+            throw new Exception(__('AuthorizeNet Payment Error: ' . $e->getMessage(), 'kirki-ecommerce'));
+        }
     }
 }
