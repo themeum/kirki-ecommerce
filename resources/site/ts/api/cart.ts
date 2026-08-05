279b743f99b870eee6c719c00ed7dcb47abc657d
Md.Harun-Ur-Rashid <harunollyo@gmail.com>
2026-08-05 16:38:21 +0600

    cart token send to add to cart api

--- resources/site/ts/api/cart.ts
+++ resources/site/ts/api/cart.ts
@@ -4,6 +4,19 @@
 
 import type { Cart, CartUpdateItem, ApiResponse } from '../types';
 import { apiRequest } from './client';
+import { Cookie } from '../cookie';
+
+function getConfig() {
+  if (!window.kirki_ecommerce) {
+    throw new Error('[kecom] window.kirki_ecommerce is not defined. Did you forget wp_localize_script?');
+  }
+  return window.kirki_ecommerce;
+}
+
+const { cart_token_header_name, cart_token_cookie_name } = getConfig();
+const headers = {
+  [cart_token_header_name]: Cookie.get(cart_token_cookie_name) || '',
+}
 
 export const cartApi = {
   get: () =>
@@ -13,6 +26,7 @@ export const cartApi = {
     apiRequest<ApiResponse<Cart>>('/cart/items', {
       method: 'POST',
       body: { variant_id: variantId, quantity },
+      headers: headers,
     }),
 
   updateItem: (itemId: number, quantity: number) =>
