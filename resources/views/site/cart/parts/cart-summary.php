6182c4776cf2be17eb92e732bcb7d31ebaa03c1c
Anindra Das Bivas <bivasdas911@gmail.com>
2026-08-07 11:56:19 +0600

    added guest cart support

--- resources/views/site/cart/parts/cart-summary.php
+++ resources/views/site/cart/parts/cart-summary.php
@@ -10,6 +10,11 @@ defined('ABSPATH') || exit;
 
 use Kirki\Ecommerce\App\Supports\Url;
 
+use function Kirki\Ecommerce\Framework\user;
+
+$checkout_url = Url::get_checkout_url();
+$login_url = Url::get_login_url($checkout_url);
+$url = user()->is_logged_in() ? $checkout_url : $login_url;
 ?>
 
 <div class="kecom-cart-summary">
@@ -30,7 +35,7 @@ use Kirki\Ecommerce\App\Supports\Url;
         <span class="kecom-cart-summary-total-title"><?php _e('Estimate Total', 'kirki-ecommerce'); ?></span>
         <span class="kecom-cart-summary-total-value" x-text="cartData.pricing.total_formatted"></span>
     </div>
-    <a href="<?php echo esc_url(Url::get_checkout_url()); ?>" class="kecom-btn kecom-btn-primary kecom-btn-block kecom-cart-summary-checkout-btn">
+    <a href="<?php echo esc_url($url); ?>" class="kecom-btn kecom-btn-primary kecom-btn-block kecom-cart-summary-checkout-btn">
         <?php _e('Proceed to Checkout', 'kirki-ecommerce'); ?>
     </a>
 </div>
--- resources/views/site/cart/parts/item/quantity.php
+++ resources/views/site/cart/parts/item/quantity.php
@@ -18,9 +18,9 @@ $max_quantity   = $data['max_quantity'] ?? 1;
 <div class="kecom-cart-item-quantity">
     <div class="kecom-quantity">
         <button class="kecom-quantity-btn" type="button" aria-label="Remove item" @click="remove(<?php echo esc_html($item['id']); ?>)" x-show="quantity === 1"> <?php Icon::render('trash'); ?></button>
-        <button class="kecom-quantity-btn" type="button" aria-label="Decrease" @click.debounce.100ms="decrement" x-show="quantity > 1"><?php Icon::render('minus'); ?></button>
-        <input class="kecom-quantity-input" type="number" :value="quantity" @input.debounce.500ms="setValue($el.value)" min="1" max="<?php echo esc_html($max_quantity); ?>" :disabled="quantity >= max" aria-label="Quantity">
-        <button class="kecom-quantity-btn" type="button" aria-label="Increase" @click.debounce.100ms="increment" :disabled="quantity >= max"> <?php Icon::render('plus'); ?></button>
+        <button class="kecom-quantity-btn" type="button" aria-label="Decrease" @click.debounce.200ms="decrement" x-show="quantity > 1"><?php Icon::render('minus'); ?></button>
+        <input class="kecom-quantity-input" type="number" :value="quantity" @input.debounce.200ms="setValue($el.value)" min="1" max="<?php echo esc_html($max_quantity); ?>" :disabled="quantity >= max" aria-label="Quantity">
+        <button class="kecom-quantity-btn" type="button" aria-label="Increase" @click.debounce.200ms="increment" :disabled="quantity >= max"> <?php Icon::render('plus'); ?></button>
     </div>
     <button class="kecom-btn kecom-btn-outline kecom-cart-item-remove" type="button" aria-label="Remove item" x-show="quantity > 1" @click="remove(<?php echo esc_html($item['id']); ?>)" :disabled="loading">
         <?php Icon::render('trash'); ?>
