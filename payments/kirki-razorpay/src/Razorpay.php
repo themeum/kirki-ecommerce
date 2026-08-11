b59995f12002a9bcd7bcacaf55b4ac69770882bb
sanjana4khan <sanjanakhan772@gmail.com>
2026-08-11 14:11:49 +0600

    Made field type required

--- payments/kirki-razorpay/src/Razorpay.php
+++ payments/kirki-razorpay/src/Razorpay.php
@@ -106,9 +106,9 @@ class Razorpay extends PaymentProvider
         parent::validate_settings($settings);
 
         Validator::make($settings, [
-            'key_id' => 'sometimes|string',
-            'key_secret' => 'sometimes|string',
-            'webhook_secret' => 'sometimes|string',
+            'key_id' => 'required|string',
+            'key_secret' => 'required|string',
+            'webhook_secret' => 'required|string',
             'sandbox' => 'sometimes|boolean',
         ])->validate();
 
