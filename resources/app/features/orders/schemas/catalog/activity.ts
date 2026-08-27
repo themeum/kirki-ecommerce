5a942e56ad3b8160d9abbd179176e39f5faae4e4
Md. Raqibul Hasan <raqibhasan.2120@gmail.com>
2026-08-27 13:22:08 +0600

    added activity type schema

--- resources/app/features/orders/schemas/catalog/activity.ts
+++ resources/app/features/orders/schemas/catalog/activity.ts
@@ -1,9 +1,30 @@
 import z from 'zod';
 
+const ActivityTypeSchema = z.enum([
+  'order-placed',
+  'payment-completed',
+  'payment-failed',
+  'processing',
+  'fulfillment-resumed',
+  'shipped',
+  'delivered',
+  'cancelled',
+  'tracking-added',
+  'archived',
+  'on-hold',
+  'partially-refunded',
+  'refunded',
+  'refund-requested',
+  'refund-deleted',
+  'comment-added',
+]);
+
+type ActivityType = z.infer<typeof ActivityTypeSchema>;
+
 const ActivitySchema = z.object({
   id: z.number(),
   order_id: z.number(),
-  activity_type: z.string(),
+  activity_type: ActivityTypeSchema,
   description: z.string(),
   created_by: z.number().nullish(),
   author_name: z.string().nullish(),
@@ -12,4 +33,4 @@ const ActivitySchema = z.object({
 
 type Activity = z.infer<typeof ActivitySchema>;
 
-export { ActivitySchema, type Activity };
+export { ActivitySchema, ActivityTypeSchema, type Activity, type ActivityType };
