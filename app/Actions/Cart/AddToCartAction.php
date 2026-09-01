e26424472e4dae91d364e47401225c8f9699e190
Anindra Das Bivas <bivasdas911@gmail.com>
2026-09-01 15:20:27 +0600

    Fix: Order limit check not getting applied when adding to cart

--- app/Actions/Cart/AddToCartAction.php
+++ app/Actions/Cart/AddToCartAction.php
@@ -27,6 +27,15 @@ class AddToCartAction
     public function execute(AddToCartDTO $dto)
     {
         $variant = $this->variant_service->find($dto->variant_id);
+        $cart = $this->cart_service->get_cart($dto->user_id, $dto->token);
+        $cart_id = $cart->id ?? 0;
+
+        $existing_item = $this->cart_service->find_item_in_cart($cart_id, $dto->variant_id);
+        $quantity = $dto->quantity;
+
+        if (!empty($existing_item)) {
+            $quantity = $existing_item->quantity + $dto->quantity;
+        }
 
         if (!$variant) {
             throw new Exception(__('Variant not found.', 'kirki-ecommerce'));
@@ -36,8 +45,8 @@ class AddToCartAction
             throw new Exception(__('Not enough stock for this variant', 'kirki-ecommerce'));
         }
 
-        if (!$this->inventory_service->is_within_limit($dto->variant_id, $dto->quantity)) {
-            throw new Exception(sprintf(__('You can not add more than %d units of this item to cart', 'kirki-ecommerce'), $dto->variant_id));
+        if (!$this->inventory_service->is_within_limit($dto->variant_id, $quantity)) {
+            throw new Exception(sprintf(__('You can not add more than %d units of this item to cart', 'kirki-ecommerce'), $variant->max_per_order));
         }
 
         $dto->product_id = $variant->product_id;
