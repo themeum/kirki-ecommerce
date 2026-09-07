bb15f58df4f6f32f73f9f4e14109356ec4bfff71
Md. Raqibul Hasan <raqibhasan.2120@gmail.com>
2026-09-07 19:23:23 +0600

    advanced page settings

---
+++ resources/app/features/settings/advanced/components/page-table.tsx
@@ -0,0 +1,9 @@
+const PageTable = () => {
+  return (
+    <div>PageTable</div>
+  );
+};
+
+PageTable.displayName = 'PageTable';
+
+export default PageTable;
--- resources/app/features/settings/advanced/pages/advanced-settings.tsx
+++ resources/app/features/settings/advanced/pages/advanced-settings.tsx
@@ -1,32 +1,59 @@
+import { Hammer, SlidersHorizontalIcon, TriangleAlert } from 'lucide-react';
+
+import Alert from '@/components/ui/alert';
+import Button from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
 import Container from '@/components/ui/container';
 import Flex from '@/components/ui/flex';
 import Text from '@/components/ui/text';
+import PageTable from '@/features/settings/advanced/components/page-table';
 import SettingsPageHeader from '@/features/settings/pages/settings-page-header';
-import { AdvancedSettingsIcon } from '@/icons';
+import { theme } from '@/theme';
 import { cardStyles } from '@/theme/card-styles';
+import { defineStyles } from '@/theme/mixins';
 import { __ } from '@/wpi18n';
+
+const AlertMessage = () => {
+  return (
+    <Flex direction="column" gap="2">
+      <Flex gap="2" align="center">
+        <TriangleAlert size="18" color={theme.colors.icon.warning} />
+        <Text color="warning" variant="heading6" weight="semibold">
+          {__('Missing/Inaccessible Pages Found', 'kirki-ecommerce')}
+        </Text>
+      </Flex>
+      <Text color="warning" variant="paragraph" weight="normal">
+        {__(
+          'Some pages are currently unavailable. Click Run Fix to automatically restore missing pages or resolve status issues.',
+          'kirki-ecommerce',
+        )}
+      </Text>
+    </Flex>
+  );
+};
 
 const AdvancedSettings = () => {
   return (
     <Container size="sm">
       <Flex direction="column" gap={4}>
         <SettingsPageHeader
-          icon={<AdvancedSettingsIcon />}
+          icon={<SlidersHorizontalIcon />}
           title={__('Advanced', 'kirki-ecommerce')}
         />
         <Card cssOverride={cardStyles.formCard}>
-          <CardContent >
+          <CardContent>
             <Flex direction="column" gap={2}>
-              <Text weight="semibold">
-                {__('Work in progress', 'kirki-ecommerce')}
-              </Text>
-              <Text color="secondary">
-                {__(
-                  'Advanced configuration options for your store will appear here.',
-                  'kirki-ecommerce',
-                )}
-              </Text>
+              <Flex justify="space-between" align="center">
+                <Text weight="semibold">{__('Pages', 'kirki-ecommerce')}</Text>
+                <Button>
+                  <Hammer size="12" />
+                  {__('Run Fix', 'kirki-ecommerce')}
+                </Button>
+              </Flex>
+              <Flex direction="column" gap={3} cssOverride={styles.contentWrapper}>
+                <Alert type="warning" text={<AlertMessage />} hasHighlight />
+                <PageTable />
+              </Flex>
             </Flex>
           </CardContent>
         </Card>
@@ -38,3 +65,9 @@ const AdvancedSettings = () => {
 AdvancedSettings.displayName = 'AdvancedSettings';
 
 export default AdvancedSettings;
+
+const styles = defineStyles({
+  contentWrapper: {
+    padding: `${theme.spacing[2]} 0}`,
+  },
+});
