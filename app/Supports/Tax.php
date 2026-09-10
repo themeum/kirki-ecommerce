4872de86e64f1867c76e1f92fe139fd3babe0344
Md.Harun-Ur-Rashid <harunollyo@gmail.com>
2026-09-10 15:47:17 +0600

    chor: tax helpers added

--- app/Supports/Tax.php
+++ app/Supports/Tax.php
@@ -2,6 +2,7 @@
 
 namespace Kirki\Ecommerce\App\Supports;
 
+use Kirki\Ecommerce\App\Supports\Facades\Settings;
 use Kirki\Ecommerce\App\Tax\Strategies\AbstractTaxStrategy;
 use Kirki\Ecommerce\App\Tax\TaxStrategyFactory;
 use Throwable;
@@ -10,9 +11,9 @@ class Tax
 {
     /**
      * Get tax strategy for a given address.
-     * 
+     *
      * @param array $address
-     * 
+     *
      * @return AbstractTaxStrategy|null
      */
     public static function get_tax_strategy($address)
@@ -26,5 +27,57 @@ class Tax
         } catch (Throwable $e) {
             return null;
         }
+    }
+
+    /**
+     * Check if tax is enabled.
+     *
+     * @since 1.0.0
+     *
+     * @return bool
+     */
+    public static function is_tax_enabled()
+    {
+        // TODO: need a dedicated `is_tax_enabled` settings.
+        return count(self::get_tax_regions()) > 0;
+    }
+
+    /**
+     * Get tax regions.
+     *
+     * @since 1.0.0
+     *
+     * @return array
+     */
+    public static function get_tax_regions()
+    {
+        return Settings::get('tax.tax_regions', []);
+    }
+
+    /**
+     * Check if tax is inclusive.
+     *
+     * @since 1.0.0
+     *
+     * @return bool
+     */
+
+    public static function is_tax_inclusive()
+    {
+        return (bool) Settings::get('tax.is_tax_inclusive_price', false);
+    }
+
+    /**
+     * Check if should show incl tax on shop page.
+     *
+     * @since 1.0.0
+     *
+     * @return bool
+     */
+    public static function should_show_incl_tax_on_shop_page()
+    {
+        return self::is_tax_enabled()
+            && self::is_tax_inclusive()
+            && (bool) Settings::get('tax.is_enabled_display_inclusive_taxed_price', false);
     }
 }
