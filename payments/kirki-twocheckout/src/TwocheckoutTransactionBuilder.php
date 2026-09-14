2427539066d21eab647c1daff2ba1b5b2a832386
sanjana4khan <sanjanakhan772@gmail.com>
2026-09-14 13:59:51 +0600

    tax issue solved for 2checkout.

--- payments/kirki-twocheckout/src/TwocheckoutTransactionBuilder.php
+++ payments/kirki-twocheckout/src/TwocheckoutTransactionBuilder.php
@@ -82,18 +82,20 @@ class TwocheckoutTransactionBuilder
         $item_names = $item_quantities = $item_prices = $item_references = $item_types = array();
 
         foreach ($this->order->items as $item) {
-            $item_names[] = html_entity_decode($item->product_name);
-            $item_quantities[] = $item->quantity;
-            $item_prices[] = PaymentProvider::format_amount($item->invoiced_price, $this->order->currency_code);
-            $item_references[] = $item->variant_id;
-            $item_types[] = TwocheckoutConstant::TYPE_PRODUCT;
-        }
+            foreach ($this->order->items as $item) {
+                $tax = $item->invoiced_tax_total ?? 0;
 
-        if (!empty($this->order->invoiced_tax_total)) {
-            $item_names[] = TwocheckoutConstant::TAX;
-            $item_quantities[] = 1;
-            $item_prices[] = PaymentProvider::format_amount($this->order->invoiced_tax_total, $this->order->currency_code);
-            $item_types[] = TwocheckoutConstant::TYPE_TAX;
+                $item_names[] = sprintf(
+                    '%s (Qty: %s%s)',
+                    html_entity_decode($item->product_name),
+                    $item->quantity,
+                    $tax > 0 ? __(' | Incl. Tax', 'kirki-ecommerce-twocheckout') : ''
+                );
+                $item_quantities[] = 1;
+                $item_prices[] = PaymentProvider::format_amount($item->invoiced_subtotal + $tax, $this->order->currency_code);
+                $item_references[] = $item->variant_id;
+                $item_types[] = TwocheckoutConstant::TYPE_PRODUCT;
+            }
         }
 
         if (!empty($this->order->invoiced_shipping_total)) {
